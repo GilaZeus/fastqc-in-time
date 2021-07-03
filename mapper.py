75275cde58e5df37148ddc299daf0fc740c40489
GilaZeus <66255227+GilaZeus@users.noreply.github.com>
2021-07-03 15:27:45 +0200

    Use pandas for csv-handling

--- mapper.py
+++ mapper.py
@@ -1,15 +1,17 @@
 # Work with standard input.
 import sys
+# Lib to parse run's metadata.
+import pandas as pd
 
 # Get the name of the sequencing technology and the date of the sample.
 acc_num = sys.stdin.readline().split(".")[0][1 :]
+csv = pd.read_csv("https://trace.ncbi.nlm.nih.gov/Traces/sra/sra.cgi?save=efetch&db=sra&rettype=runinfo&term=" + acc_num)
 
-csv = urllib.request.urlopen("https://trace.ncbi.nlm.nih.gov/Traces/sra/sra.cgi?save=efetch&db=sra&rettype=runinfo&term=" + acc_num)
-csv.readline() 
-csv = csv.readline().split(",")
-name = csv[1]
-date = csv[10]
+date = csv["ReleaseDate"][0][ : 10]
+name = csv["LibraryStrategy"][0]
 
+print(name)
+print(date)
 # every forth line is interesting for us; create a counter.
 i = 1
 
@@ -18,7 +20,7 @@ for line in sys.stdin:
         # Information on Phred-score encoding in FASTQ-files:
         # http://people.duke.edu/~ccc14/duke-hts-2018/bioinformatics/quality_scores.html
         for char in line:
-            print(",".join([name, date, ord(char) - 33, 1]))
+            print(",".join([name, date, str(ord(char) - 33), "1"]))
         i = 0
     else:
         i += 1
