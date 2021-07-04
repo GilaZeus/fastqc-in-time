8b9db06a83f8da01e69f1f5bba5296532294e180
GilaZeus <66255227+GilaZeus@users.noreply.github.com>
2021-07-04 07:28:24 +0200

    Add reduce function

---
+++ reducer.py
@@ -0,0 +1,36 @@
+# Work with standard input.
+import sys
+# Libs to sort input.
+from itertools import groupby
+from operator import itemgetter
+
+def read_mapper_output(data):
+    """Reads output of a mapper.
+    
+    Espects "library strategy,year-month,Phred-score,count",
+    returns them splitted by comma."""
+    for line in data:
+        yield line.strip().split(",")
+
+
+def main():
+    """Reduces mapper's output. Calculates average Phred-score for each month.
+    Ignores wrong input.
+    
+    Output: "Sequencing technology,date,average Phred-score"."""
+    data = read_mapper_output(sys.stdin)
+    for current_month, group in groupby(data, itemgetter(0, 1)):
+        try:
+            total_phred = 0
+            total_len = 0
+            for item in group:
+                total_phred += int(item[-2])
+                total_len += int(item[-1])
+            print(",".join([item[0], item[1], str(total_phred / total_len)]))
+        except:
+            pass
+        
+
+
+if __name__ == "__main__":
+    main()
