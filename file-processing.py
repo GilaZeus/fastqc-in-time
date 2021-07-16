6e9aa83c5e82537d4fe6fec075347d1807240287
GilaZeus <gilyazevr@gmail.com>
2021-07-16 02:15:12 +0200

    Update file-processing.py

--- file-processing.py
+++ file-processing.py
@@ -1,10 +1,11 @@
+#!/usr/bin/env python3
 import datetime
 import accnums
 
 
 def main():
     start_date = datetime.date(2021, 6, 30)
-    end_date = datetime.date(2020, 6, 29)
+    end_date = datetime.date(2020, 1, 1)
     delta = datetime.timedelta(days=1)
 
     while start_date >= end_date:
