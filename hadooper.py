000d988e187f3f096fba0da0078f25062b3fe44d
GilaZeus <gilyazevr@gmail.com>
2021-08-14 20:27:22 +0200

    Create hadooper.py

---
+++ hadooper.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""Run hadoop separately for each three months."""
+import subprocess
+import os
+
+
+def hadoop_call(input_folder="tmp", result_folder="result",
+                mapper="fastqc-in-time/mapper_old.py",
+                reducer="fastqc-in-time/reducer.py"):
+    """Call hadoop for data in input_folder, safe it under result_folder."""
+   
+    subprocess.run(["hadoop", "jar", "hadoop-3.3.1/share/hadoop/tools/lib/hadoop-streaming-3.3.1.jar", 
+                    "-archives", "fastq.tar", "-file", mapper, "-file", reducer,
+                    "-mapper", mapper[mapper.find("/") + 1 :], "-reducer", reducer[reducer.find("/") + 1 :],
+                    "-input", input_folder + "/*",  "-output", result_folder])
+
+
+def hadoop_chain(input_folder="tmp", result_folder="result",
+                 mapper="fastqc-in-time/mapper_old.py",
+                 reducer="fastqc-in-time/reducer.py"):
+    """Chain hadoop for subfolders in folder."""
+    
+    subfolders = os.listdir(input_folder)
+    for subfolder in subfolders:
+        if os.path.isdir(subfolder):
+            hadoop_call(input_folder=input_folder + "/" + subfolder,
+                        result_folder=result_folder + "/" + subfolder,
+                        mapper=mapper, reducer=reducer)
+
+
+def main():
+    input_folder, result_folder = sys.argv[1 :]
+    hadoop_chain(input_folder=input_folder, result_folder=result_folder)
+
+
+if __name__ == "__main__":
+    main()
