510d0077e66f1fed30530ce68e28e399f7082553
GilaZeus <gilyazevr@gmail.com>
2021-08-14 16:42:45 +0200

    Remove exception handling as test

--- mapper_old.py
+++ mapper_old.py
@@ -24,32 +24,28 @@ def map_fastq(entry):
     
     Single line of output: "technology name,date,phred score,count"."""
     name, date, acc_num = entry.split(",")
-    try:
-        process = subprocess.Popen(["fastq-dump-orig.2.11.0", "-Z", acc_num], stdout=subprocess.PIPE, bufsize=-1)
-        total_phred = 0
-        total_len = 0
-        i = 0
-        while True:
-            line = process.stdout.readline()
-            line = line.strip().decode("utf-8")
-
-            if line == "":
-                break
-        
-            if i == 3:
-                # Information on Phred-score encoding in FASTQ-files:
-                # http://people.duke.edu/~ccc14/duke-hts-2018/bioinformatics/quality_scores.html
-                total_phred += reduce(lambda x, y: x + y, map(lambda x: ord(x) - 33, line))
-                total_len += len(line)
-            i += 1
-            if i == 4:
-                i = 0
     
-        print(",".join([name, date, str(total_phred), str(total_len)]))
-    except OSError:
-        pass
-    finally:
-        process.kill()
+    process = subprocess.Popen(["fastq-dump-orig.2.11.0", "-Z", acc_num], stdout=subprocess.PIPE, bufsize=-1)
+    total_phred = 0
+    total_len = 0
+    i = 0
+    while True:
+        line = process.stdout.readline()
+        line = line.strip().decode("utf-8")
+        if line == "":
+            break
+    
+        if i == 3:
+            # Information on Phred-score encoding in FASTQ-files:
+            # http://people.duke.edu/~ccc14/duke-hts-2018/bioinformatics/quality_scores.html
+            total_phred += reduce(lambda x, y: x + y, map(lambda x: ord(x) - 33, line))
+            total_len += len(line)
+        i += 1
+        if i == 4:
+            i = 0
+
+    print(",".join([name, date, str(total_phred), str(total_len)]))
+    process.kill()
 
 
 def main():
